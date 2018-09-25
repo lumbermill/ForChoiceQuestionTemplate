bad3134ea34451cf86c92efb2c5d34ffa2566c8f
Kai Shoya <s-kai@lumber-mill.co.jp>
2018-09-25 16:29:47 +0900

    メニュー用のListViewを追加

--- app/src/main/java/jp/co/lumber_mill/fourchoicequestiontemplate/MainActivity.kt
+++ app/src/main/java/jp/co/lumber_mill/fourchoicequestiontemplate/MainActivity.kt
@@ -2,11 +2,18 @@ package jp.co.lumber_mill.fourchoicequestiontemplate
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val listView = findViewById<ListView>(R.id.list_view)
+        val items = arrayOf("anime", "game")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
+        listView.adapter = adapter
     }
 }
