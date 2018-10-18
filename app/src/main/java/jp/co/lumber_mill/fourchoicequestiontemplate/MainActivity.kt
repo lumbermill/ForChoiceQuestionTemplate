e4170a67ab260f52b913742ab58d52c15cabe3fe
Kai Shoya <s-kai@lumber-mill.co.jp>
2018-10-18 18:05:25 +0900

    コメントを追加しました

--- app/src/main/java/jp/co/lumber_mill/fourchoicequestiontemplate/MainActivity.kt
+++ app/src/main/java/jp/co/lumber_mill/fourchoicequestiontemplate/MainActivity.kt
@@ -12,12 +12,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // xmlのListViewを取得します
         val listView = findViewById<ListView>(R.id.list_view)
+        // Context.ktで作成したquestionsのキーの名前を取得します
         val items = Context.questions.keys.toTypedArray()
+        // simple_list_item_1というListViewのテンプレートを使用します
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items)
         listView.adapter = adapter
+        // listViewのアイテムをクリックした時の動きを登録します
         listView.setOnItemClickListener { adapterView, view, i, l ->
+            // Intentを使いQuizActivityを表示します
             val intent = Intent(this, QuizActivity::class.java)
+            // ListViewの何番目がクリックされたのかをidという名前でquizActivityに渡します
             intent.putExtra("id", i)
             startActivity(intent)
         }
--- app/src/main/java/jp/co/lumber_mill/fourchoicequestiontemplate/QuizActivity.kt
+++ app/src/main/java/jp/co/lumber_mill/fourchoicequestiontemplate/QuizActivity.kt
@@ -7,24 +7,38 @@ import android.widget.Button
 import android.widget.ImageView
 
 class QuizActivity : AppCompatActivity(), View.OnClickListener {
+    // 使用する問題を入れておく変数
     private lateinit var questions: ArrayList<ArrayList<String>>
+
+    // 問題の数をカウントする変数
     private var index: Int = 0
+
+    // 次の問題を表示するためのボタン
     private lateinit var next: Button
+
+    // マル・バツを表示するImageView
     private lateinit var judge: ImageView
+
+    // 正解を入れておく変数
     private var correct: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
 
+        // MainActivityから`id`を取得
         val id = intent.getIntExtra("id", 0)
+        // Context.ktで作成したquestionsのキーの名前を取得します
         val keys = Context.questions.keys.toTypedArray()
+        // idとキーの名前から問題を取得します
         questions = Context.questions[keys[id]]!!
+        // 毎回違う順番で問題を出すため、取得した問題の順番をシャッフルします
         questions.shuffle()
 
         next = findViewById(R.id.next)
         next.setOnClickListener {
             if (questions.count() <= index) {
+                // アクティビティを終了します
                 finish()
             } else {
                 changeView(questions[index])
@@ -36,44 +50,64 @@ class QuizActivity : AppCompatActivity(), View.OnClickListener {
         changeView(questions[index])
     }
 
+    /**
+     * 表示する問題を入れ替える関数
+     * @param question
+     */
     private fun changeView(question: ArrayList<String>) {
+        // 問題用の画像を表示するImageViewを取得します
         val imageView = findViewById<ImageView>(R.id.imageView)
+
+        // 選択肢1〜4までのボタンを取得します
         val btn1 = findViewById<Button>(R.id.button1)
         val btn2 = findViewById<Button>(R.id.button2)
         val btn3 = findViewById<Button>(R.id.button3)
         val btn4 = findViewById<Button>(R.id.button4)
 
+        // 次の問題を表示するボタンを無効にし、マル・バツを透明にします
         next.isEnabled = false
         judge.alpha = 0f
 
+        // 問題の正解を入れておきます
+        // 配列の0番目を正解とします
         correct = question[0]
+        // 正解と同じ名前の画像を取得し、ImageViewに表示します
         val resId = resources.getIdentifier(correct, "drawable", packageName)
         imageView.setImageResource(resId)
-        question.shuffle()
 
+        // 問題をシャッフルしボタンに表示します
+        question.shuffle()
         btn1.text = question[0]
         btn2.text = question[1]
         btn3.text = question[2]
         btn4.text = question[3]
 
-
+        // ボタンをタップした時の動作を登録します
         btn1.setOnClickListener(this)
         btn2.setOnClickListener(this)
         btn3.setOnClickListener(this)
         btn4.setOnClickListener(this)
     }
 
+    /**
+     * 選択肢のボタンをタップした際に呼び出される関数
+     */
     override fun onClick(p0: View?) {
+        // タップされたボタンの文字を取得します
         val answer = (p0 as Button).text as String
 
-        index += 1
-        next.isEnabled = true
-        judge.alpha = 1f
-
+        // 正誤判定をし、マル・バツの画像をjudgeにセットします
         if (answer == correct) {
             judge.setImageResource(R.drawable.correct)
         } else {
             judge.setImageResource(R.drawable.wrong)
         }
+
+        // 問題のカウントを+1します
+        index += 1
+        // 透明にしていたマル・バツの画像を表示します
+        judge.alpha = 1f
+        // 次の問題を表示するボタンを有効にします
+        next.isEnabled = true
     }
 }
